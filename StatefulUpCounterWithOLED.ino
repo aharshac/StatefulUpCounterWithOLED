227b9f6630b46d697b238623f38d6678bd6a7818
aharshac <aharshac@gmail.com>
2017-12-09 21:20:15 +0530

    Switched to ASCII OLED library

--- StatefulUpCounterWithOLED.ino
+++ StatefulUpCounterWithOLED.ino
@@ -1,42 +1,44 @@
 // Library imports
 #include <Bounce2.h>  // Contact bouncing 
 #include <EEPROM.h>   
-#include <Adafruit_GFX.h>
-#include <Adafruit_SSD1306.h>
 
+#include <SSD1306Ascii.h>
+#include <SSD1306AsciiAvrI2c.h>
 
 
 /* Constants and variables */
-const int OLED_RESET = 4;
-
-const int MAX_RAM = 2048; // Bytes
+const PROGMEM unsigned int MAX_RAM = 2048; // Bytes
 const PROGMEM char STR_FREE_RAM[] = "Free Memory = %d %%";
 
-const PROGMEM char SEPARATOR[] = "-------------------------------------";   // Line separator for Serial console
 
-// Clear OLED line without clearing whole screen
-const PROGMEM char CLEAR_FONT_SIZE_1[] = "                             ";   // 128/6 = 29 whitespace characters
-const PROGMEM char CLEAR_FONT_SIZE_2[] = "          ";   // 128/12 = 10 whitespace characters
+// Line separator for Serial console
+const PROGMEM char SEPARATOR[] = "-------------------------------------";
 
+
+// OLED rows and columns
+const PROGMEM unsigned int TITLE_ROW = 0;     // pixels/8
+const PROGMEM unsigned int TITLE_COL = 0;     // pixels
+const PROGMEM unsigned int SUBTITLE_ROW = 1;
+const PROGMEM unsigned int SUBTITLE_COL = 0;
+const PROGMEM unsigned int PLUS_ROW = 2;
+const PROGMEM unsigned int PLUS_COL = 0;
+const PROGMEM unsigned int COUNT_ROW = 2;
+const PROGMEM unsigned int COUNT_COL = 60;
 
 
 // Used GPIO pins
-const int pinUp = 9;
-const int pinReset = 2;
-const int pinLed = 13;
-
+const PROGMEM unsigned int pinUp = 9;
+const PROGMEM unsigned int pinReset = 2;
+const PROGMEM unsigned int pinLed = 13;
 
 
 // Button Objects
 Bounce buttonUp = Bounce(); 
 Bounce buttonReset = Bounce(); 
-Adafruit_SSD1306 display(OLED_RESET);
-
-#if (SSD1306_LCDHEIGHT != 32)
-  #error("Height incorrect, please fix Adafruit_SSD1306.h!");
-#endif
 
 
+// OLED
+SSD1306AsciiAvrI2c oled;
 
 // Statefulness
 bool stateLed = false;
@@ -68,77 +70,110 @@ void CheckAndLimitCounter() {
 
 
 // Output
-void ToggleLed() {
+void LED_Toggle() {
   stateLed = !stateLed;
   digitalWrite(pinLed, stateLed ? HIGH : LOW);
 }
 
 
-void SerialPrintSeparator() {
+void Serial_PrintSeparator() {
   Serial.println((__FlashStringHelper*)SEPARATOR);
 }
 
-void ClearLine(int y = 0, int fontSize = 1) {
-  display.setTextSize(fontSize == 2 ? 2 : 1);
-  display.setTextColor(0xFFFF, 0);
-  display.setCursor(0, y);
-  display.print("      ");
-  display.display();
+void Display_ClearLine(int row, int startCol = 0, bool size2x = false) {
+  if (!size2x) 
+    oled.set1X();
+  else
+    oled.set2X();
+  oled.setCursor(startCol, row);
+  oled.clearToEOL();
   delay(5);
 }
 
-void DisplaySplashScreen() {
-  display.setTextSize(1);
-  display.setTextColor(WHITE);
-  display.setCursor(0,0);
-  display.println("COLLABORIZM");
-  display.display();
-  delay(5);
-
-  display.setTextSize(1);
-  display.setCursor(0,8);
-  display.println("STATEFUL");
-  display.display();
-  delay(5);
-
-  display.setTextSize(2);
-  display.setCursor(0,18);
-  display.println("UP COUNTER");
-  display.display();
-  delay(2000);
-}
-
-
-void DisplayFreeRam() {
-  unsigned int freePercent = (GetFreeRam() / MAX_RAM) * 100;
-
-  //char buffer[strlen(STR_FREE_RAM) + 3];
-  //sprintf(buffer, STR_FREE_RAM, freePercent);
+void Display_SplashScreen() {
+  Serial.println(F(""));
+  Serial.println(F("Stateful Up Counter With OLED Display"));
   
-  ClearLine(0, 1);
+  oled.set1X();
+  oled.setCursor(TITLE_COL, TITLE_ROW);
+  oled.println("COLLABORIZM");
+  delay(5);
 
-  /*
-  display.setCursor(0,0);
-  display.println(buffer);
-  display.display();
-  delay(5);*/
+  oled.setCursor(SUBTITLE_COL, SUBTITLE_ROW);
+  oled.println("STATEFUL");
+  delay(5);
+
+  oled.set2X();
+  oled.setCursor(0, COUNT_ROW);
+  oled.println("UP COUNTER");
+}
+
+
+void Display_FreeRam() {
+  unsigned int freePercent = (GetFreeRam() / (float)MAX_RAM) * 100;
+
+  char buffer[19];
+  sprintf(buffer, "Free Memory = %d%%", freePercent);
+
+  Serial.println(buffer);
+  Display_ClearLine(SUBTITLE_ROW);
+
+  oled.set1X();
+  oled.setCursor(SUBTITLE_COL, SUBTITLE_ROW);
+  oled.println(buffer);
+  delay(5);
+}
+
+
+void Display_Count() {
+  char buffer[6];
+  sprintf(buffer, "%5d", counter.count);
+
+  Display_ClearLine(COUNT_ROW, COUNT_COL, true);
+  
+  oled.set2X();
+  oled.setCursor(COUNT_COL, COUNT_ROW);
+  oled.print(buffer);
+  delay(5);
+}
+
+
+void Display_PlusSign() {
+  // Display_ClearLine(COUNT_ROW, COUNT_COL, true);
+  
+  oled.set2X();
+  oled.setCursor(PLUS_COL, PLUS_ROW);
+  oled.clear(PLUS_COL, COUNT_COL - 1, PLUS_ROW, PLUS_ROW + 1);
+  delay(5);
+
+  oled.set2X();
+  oled.print("+");
+  delay(100);
+  
+  oled.set2X();
+  oled.setCursor(PLUS_COL, PLUS_ROW);
+  oled.clear(PLUS_COL, COUNT_COL - 1, PLUS_ROW, PLUS_ROW + 1);
+  delay(50);
+}
+
+
+void Display_Reset() {
+  Display_ClearLine(SUBTITLE_ROW);
+  Display_ClearLine(COUNT_ROW, COUNT_COL, true);
+   
+  oled.set2X();
+  oled.setCursor(COUNT_COL, COUNT_ROW);
+  oled.println("RESET");
+  delay(50);
 }
 
 void setup() {
   Serial.begin(9600);
   
-  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);  // initialize with the I2C addr 0x3C (for the 128x32)
-  display.clearDisplay();
+  oled.begin(&Adafruit128x32, 0x3C);
+  oled.setFont(Adafruit5x7);  
 
-  Serial.println(F(""));
-  Serial.println(F("Stateful Up Counter With OLED Display"));
-  
-  DisplaySplashScreen();
-  
-  Serial.print(F("Free RAM (bytes) = "));
-  Serial.println(GetFreeRam());
-
-  DisplayFreeRam();
+  Display_SplashScreen();
 
   Serial.println(F("Initializing Hardware"));
   pinMode(pinLed, OUTPUT);
@@ -153,11 +188,18 @@ void setup() {
   Serial.println(F("Reading EEPROM"));
   EEPROM.get(0, counter);
   CheckAndLimitCounter();
+  
   Serial.print(F("Stored count = "));
   Serial.println(counter.count);
 
+  delay(1000);
+  Display_FreeRam();
+
+  Display_ClearLine(COUNT_ROW, 0, true);
+  Display_Count();
+
   Serial.println(F("Ready..."));
-  SerialPrintSeparator();
+  Serial_PrintSeparator();
 }
  
  
@@ -171,7 +213,7 @@ void loop() {
   if (prevStateUp == pressUp && prevStateReset == pressReset) {
     return;
   } else {
-    ToggleLed();
+    LED_Toggle();
   }
         
   prevStateUp = pressUp;
@@ -184,24 +226,31 @@ void loop() {
     CheckAndLimitCounter();
     counter.count += 1;
     Serial.println(counter.count);
+
+    Display_PlusSign();
+    Display_Count();
     
     Serial.println(F("Saving to EEPROM"));
     EEPROM.put(0, counter);
 
-    SerialPrintSeparator();
-    delay(500);
+    Serial_PrintSeparator();
+    delay(300);
     return;
   }
 
   if (pressReset && !pressUp) {
     Serial.println(F("Reset button pressed."));
 
+    Display_Reset();
+    
     Serial.println(F("Clearing EEPROM"));
     counter.count = 0;
     EEPROM.put(0, counter);
     
     Serial.println(F("Restarting..."));
     delay(1000);
+    
+    oled.clear();
     ResetNow();
   }
 }
